fd49ebf1e294fbce69ae1502e5f51aff2ec284ff
ssjisan <sakibfset13@gmail.com>
2022-12-06 22:27:09 +0600

    all icons are set with dynamic colors

--- src/Assets/Icons/Reports.js
+++ src/Assets/Icons/Reports.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 export default function Reports() {
+  const { pathname } = useLocation();
+
   return (
     <svg
       width="22"
@@ -11,28 +14,28 @@ export default function Reports() {
     >
       <path
         d="M7.33325 15.5833H14.6666"
-        stroke="#F53062"
+        stroke={pathname === "/report" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M7.33325 11.9167H12.8333"
-        stroke="#F53062"
+        stroke={pathname === "/report" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M8.24992 4.125H4.58325V19.25H17.4166V4.125H13.7499"
-        stroke="#F53062"
+        stroke={pathname === "/report" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M13.75 2.75H8.25V4.125V5.5H13.75V4.125V2.75Z"
-        stroke="#F53062"
+        stroke={pathname === "/report" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
--- src/Assets/Icons/Zone.js
+++ src/Assets/Icons/Zone.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 export default function Zone() {
+  const { pathname } = useLocation();
+
   return (
     <svg
       width="22"
@@ -11,21 +14,21 @@ export default function Zone() {
     >
       <path
         d="M13.75 2.75V17.4167"
-        stroke="#F53062"
+        stroke={pathname === "/zone" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M8.25 4.58325V19.2499"
-        stroke="#F53062"
+        stroke={pathname === "/zone" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M2.75 2.75L8.25 4.58333L13.75 2.75L19.25 4.58333V19.25L13.75 17.4167L8.25 19.25L2.75 17.4167V2.75Z"
-        stroke="#F53062"
+        stroke={pathname === "/zone" ? "#F53062" : "#757575"}
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
