a2ccf35f1f1e5dded94c29633bb45d4066ba313b
ssjisan <sakibfset13@gmail.com>
2022-12-08 17:02:32 +0600

    route assigned

--- src/Components/MainRoute.js
+++ src/Components/MainRoute.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import Products from "../Pages/Products/Products";
+import Salesman from "../Pages/Salesman/Salesman";
+import Zone from "../Pages/Zone/Zone";
+import DailySales from "../Pages/DailySales/DailySales";
+import Reports from "../Pages/Reports/Reports";
 
 export default function MainRoute() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/product" element={<Products />} />
+      <Route path="/salesman" element={<Salesman />} />
+      <Route path="/zone" element={<Zone />} />
+      <Route path="/daily-sales" element={<DailySales />} />
+      <Route path="/report" element={<Reports />} />
     </Routes>
   );
 }
